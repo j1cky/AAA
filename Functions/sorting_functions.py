0a36f3b3586c68b38dd7e5a508d255bc0cec55d8
J1cky <j1cky@users.noreply.github.com>
2024-11-15 14:56:08 +0100

    Automate Apply for Engineering Job

--- Functions/sorting_functions.py
+++ Functions/sorting_functions.py
@@ -28,65 +28,11 @@ options.add_argument('--disable-dev-shm-usage')
 # options.add_argument("--profile-directory=Default") 
 
 
-def offer_applier(test_url,usr, password,cv_path,message):
-
-    driver = check_easy_apply(test_url)
-
-    if driver:
-        formulaire = promotion_or_formulaire(driver)
-
-        if not formulaire:
-            if connected(driver) :
-                postuler(driver)
-                fill_and_submit(driver, cv_path, message)
-            else :
-                # print('Lien du formulaire')
-                connexion = apec_connect(driver,usr, password)
-                if connexion :
-                    print('Connected')
-                    fill_and_submit(driver, cv_path, message)
-                
-        else:
-            fill_and_submit(driver, cv_path, message)
-
-
-        driver.quit()
-        print("Submitted, everything fine")
-        return None
-
-
-
-def select_importer_cv(driver):
-    try:
-        wait = WebDriverWait(driver, 10)
-        
-        # SClick the "Apply with CV" radio button
-        apply_with_cv_radio = wait.until(
-            EC.element_to_be_clickable((By.XPATH, "//div[contains(@class, 'form-check') and .//label[contains(text(), 'Je préfère joindre uniquement un CV')]]//input"))
-        )
-        apply_with_cv_radio.click()
-
-        # Step 2: Wait for the "Import a CV" option to be visible (this assumes it appears after the first option is selected)
-        dropdown_options = wait.until(
-            EC.presence_of_all_elements_located((By.XPATH, "//div[contains(@class, 'dropdown') or contains(@class, 'form-check')]//label"))
-        )
-
-        # Loop through options to find "Importer un CV"
-        for option in dropdown_options:
-            text = option.text.strip()
-            if "Importer un CV" in text:
-                option.click()
-                break
-        
-    except Exception as e:
-        print(f"Error selecting 'Apply with CV' and 'Import a CV': {e}")
 
 
 def fill_and_submit(driver, cv_path, message):
-
     try:
         wait = WebDriverWait(driver, 10)
-
         if message :
             pass
 
@@ -144,83 +90,64 @@ def fill_and_submit(driver, cv_path, message):
     except (TimeoutException, NoSuchElementException) as e:
         print(f"Error during the application process: {e}")
 
-
-def read_login_credentials(filepath):
-    """
-    Reads email and password from a specified text file.
-        filepath (str): Path to the login file.
-
-    Returns:
-        tuple: (email, password)
-    """
-    filepath = os.path.expanduser(filepath)
-
-    # Read the file and extract email and password
-    try:
-        with open(filepath, 'r') as file:
-            lines = file.read().splitlines()
-            email = lines[0].strip()  # First line is the email
-            password = lines[1].strip()  # Second line is the password
-            return email, password
-    except FileNotFoundError:
-        print("Error: File not found.")
-        return None, None
-    
-    except IndexError:
-        print("Error: File format is incorrect. String! First line email, second line password.")
-        return None, None
-    
-
-def postuler(driver):
+def select_importer_cv(driver):
     try:
         wait = WebDriverWait(driver, 10)
-        # Click on the "Postuler" button
-        connect_button = wait.until(
-            EC.element_to_be_clickable((By.XPATH, "(//button[@type='submit' and contains(@class, 'popin-btn')])[2]"))
+        
+        # Click "Apply with CV" radio button
+        apply_with_cv_radio = wait.until(
+            EC.element_to_be_clickable((By.XPATH, "//div[contains(@class, 'form-check') and .//label[contains(text(), 'Je préfère joindre uniquement un CV')]]//input"))
         )
+        apply_with_cv_radio.click()
+
+        # Wait for "Import a CV" 
+        dropdown_options = wait.until(
+            EC.presence_of_all_elements_located((By.XPATH, "//div[contains(@class, 'dropdown') or contains(@class, 'form-check')]//label"))
+        )
+
+        # Loop through options to find "Importer un CV"
+        for option in dropdown_options:
+            text = option.text.strip()
+            if "Importer un CV" in text:
+                option.click()
+                break
+        
     except Exception as e:
-        print(f"Error checking first apply button: {e}")
+        print(f"Error selecting 'Apply with CV' and 'Import a CV': {e}")
         return False
 
 
-def connected(driver):
-    """
-    Checks if the user is already connected by looking at the navigation bar.
-    - Returns True if the user is logged in.
-    - Returns False if the user is not logged in.
-    """
+# ----------------------------------------------------------------------------------------------- #
+# ----------------------------------------------------------------------------------------------- #
+
+
+def already_applied(driver):
     try:
         wait = WebDriverWait(driver, 5)
-
-        # Check for the logged-in link (with class 'logged')
-        logged_in_element = wait.until(
-            EC.presence_of_element_located((By.XPATH, "//a[contains(@class, 'nav-link-espace')]"))
+        
+        # Check if the "Vous avez déjà postulé à cette offre" message is present
+        already_applied_message = wait.until(
+            EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'alread-applied') and contains(., 'Vous avez déjà postulé à cette offre')]"))
         )
-
-        # Determine if the user is logged in or not
-        if "logged" in logged_in_element.get_attribute("class"):
-            username = logged_in_element.find_element(By.TAG_NAME, "span").text
-            # print(f"User is connected: {username}")
+        
+        # If the element is found, the user has already applied
+        if already_applied_message:
+            print("Great !! I already applied to this job.")
             return True
-        else:
-            # print("User is NOT connected (Mon espace).")
-            return False
-
-    except Exception as e:
-        print(f"Error checking connection status: {e}")
+        
         return False
 
-
-def promotion_or_formulaire(driver):
-    current_url = driver.current_url
-    # Check if the user is on the promotion URL (not connected)
-    if "promotion" in current_url:
+    except TimeoutException:
+        print("Application possibility.")
         return False
     
-    # Check if the user is on the formulaire URL (connected)
-    if "formulaire" in current_url:
-        print("User is connected.")
-        return True
+    except Exception as e:
+        print(f"Error verifying whether applied or not: {e}")
+        return None
+
+
+# ----------------------------------------------------------------------------------------------- #
+# ----------------------------------------------------------------------------------------------- #
 
 
 def apec_connect(driver, usr, password):
@@ -261,59 +188,67 @@ def apec_connect(driver, usr, password):
         print(f"Erreur lors de la connexion : {e}")
         return False
 
+# ----------------------------------------------------------------------------------------------- #
+# ----------------------------------------------------------------------------------------------- #
 
 def check_easy_apply(url):
-
     try:
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-        wait = WebDriverWait(driver, 6)
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)        
         driver.get(url)
-
-        #  Cookie Handling
-        try:
-            cookie_wait = WebDriverWait(driver, 2)
-            close_banner = cookie_wait.until(
-                EC.element_to_be_clickable((By.ID, "onetrust-accept-btn-handler"))
-            )
-            close_banner.click()
-            # print("Closed cookie consent banner.")
-        except TimeoutException:
-            # print("No cookie consent banner found, continuing immediately.")
-            pass
-        # Locate the "Postuler" button
-        try:            
-            apply_button = wait.until(
-                EC.presence_of_element_located(
-                    (By.XPATH, "//a[contains(@class, 'btn-primary') and contains(text(), 'Postuler')]")
-                )
-            )
-            button_text = apply_button.text.strip()
-
-            # Easy Apply number 0 
-            if button_text == "Postuler":
-                # print("Easy Apply button found.")
-                apply_button.click()                
-                time.sleep(2)  
-                return driver        
-
-            # Website 
-            elif "Postuler sur le site" in button_text:
-                print("Complicated Apply button found (redirects to external site).")
-                return False
-            
-            # No longer Available number 2 
-            else:
-                print("Unknown or no longer available")
-                return False
-            
-        except (TimeoutException, Exception) :
-            # print("No apply button found.")
-            driver.quit()
-            return False   
+        cookie_handling(driver)   
+        driver = locate_and_click_postuler_button(driver) # availability is true (= driver) when postuler button exists, i give it 6 seconds
+        
+        return driver
+        
     except Exception as e:
         print(f"Error during checking apply possibility: {e}")
-        driver.quit()
-        return False
+        return False 
+
+def cookie_handling(driver):
+    try:
+        cookie_wait = WebDriverWait(driver, 2)
+        close_banner = cookie_wait.until(
+            EC.element_to_be_clickable((By.ID, "onetrust-accept-btn-handler"))
+        )
+        close_banner.click()
+        # print("Closed cookie consent banner.")
+    except TimeoutException:
+        # print("No cookie consent banner found, continuing immediately.")
+        pass
+
+def locate_and_click_postuler_button(driver):
+    try:            
+        wait = WebDriverWait(driver,5)
+        apply_button = wait.until(
+            EC.presence_of_element_located(
+                (By.XPATH, "//a[contains(@class, 'btn-primary') and contains(text(), 'Postuler')]")
+            )
+        )
+        button_text = apply_button.text.strip()
+
+        # Postuler button available
+        if button_text == "Postuler":
+            # print("Easy Apply button found.")
+            apply_button.click()                
+            time.sleep(2)  
+            return driver        
+
+        # Website button available
+        elif "Postuler sur le site" in button_text:
+            print("Complicated Apply button found (redirects to external site).")
+            return False
+        
+        # Already applied 
+        else:
+            print("Unknown or no longer available")
+            return False
+        
+    except (TimeoutException, Exception) :
+        print("No apply button found.")
+        return False  
+
+# ----------------------------------------------------------------------------------------------- #
+# ----------------------------------------------------------------------------------------------- #
 
 
 def get_job_links(url):
@@ -346,3 +281,87 @@ def get_job_links(url):
         return None
 
 
+def read_login_credentials(filepath):
+    """
+    Reads email and password from a specified text file.
+        filepath (str): Path to the login file.
+
+    Returns:
+        tuple: (email, password)
+    """
+    filepath = os.path.expanduser(filepath)
+
+    # Read the file and extract email and password
+    try:
+        with open(filepath, 'r') as file:
+            lines = file.read().splitlines()
+            email = lines[0].strip()  # First line is the email
+            password = lines[1].strip()  # Second line is the password
+            return email, password
+    except FileNotFoundError:
+        print("Error: File not found.")
+        return None, None
+    
+    except IndexError:
+        print("Error: File format is incorrect. String! First line email, second line password.")
+        return None, None
+    
+
+
+# ----------------------------------------------------------------------------------------------- #
+# ---------------------------------        Trash        ----------------------------------------- #
+# ----------------------------------------------------------------------------------------------- #
+
+
+def click_on_postuler(driver):
+    try:
+        wait = WebDriverWait(driver, 10)
+        # Click on the "Postuler" button
+        connect_button = wait.until(
+            EC.element_to_be_clickable((By.XPATH, "(//button[@type='submit' and contains(@class, 'popin-btn')])[2]"))
+        )
+        connect_button.click()
+
+    except Exception as e:
+        print(f"Error checking first apply button: {e}")
+        return False
+
+
+def promotion_or_formulaire(driver):
+    current_url = driver.current_url
+    # Check if the user is on the promotion URL (not connected)
+    if "promotion" in current_url:
+        return False
+    
+    # Check if the user is on the formulaire URL (connected)
+    if "formulaire" in current_url:
+        print("User is connected.")
+        return True
+
+
+def connected(driver):
+    """
+    Checks if the user is already connected by looking at the navigation bar.
+    - Returns True if the user is logged in.
+    - Returns False if the user is not logged in.
+    """
+    try:
+        wait = WebDriverWait(driver, 5)
+
+        # Check for the logged-in link (with class 'logged')
+        logged_in_element = wait.until(
+            EC.presence_of_element_located((By.XPATH, "//a[contains(@class, 'nav-link-espace')]"))
+        )
+
+        # Determine if the user is logged in or not
+        if "logged" in logged_in_element.get_attribute("class"):
+            username = logged_in_element.find_element(By.TAG_NAME, "span").text
+            # print(f"User is connected: {username}")
+            return True
+        else:
+            # print("User is NOT connected (Mon espace).")
+            return False
+
+    except Exception as e:
+        print(f"Error checking connection status: {e}")
+        return False
